d89b02eb271adde8a19da4c9310fd94a184d8466
Francis Miguel Abella <migyn.s92mike@gmail.com>
2018-06-25 14:47:49 +0800

    Create appss.js

---
+++ js/appss.js
@@ -0,0 +1,17 @@
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        Hellow World ssss!!!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('main')
+)
+
+// const getMessage = () => "Hello World";
+// document.getElementById('main').innerHTML = getMessage();
